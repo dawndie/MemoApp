2b1a49132852cfcabcca8e3a9e3e54ef252f1cc3
Bang.Voluong <vo.luong.bang@sun-asterisk.com>
2025-10-12 01:19:54 +0900

    fix: update dockerfile

--- MemoApp_Backend/app/src/main/java/memoapp/config/CorsConfig.java
+++ MemoApp_Backend/app/src/main/java/memoapp/config/CorsConfig.java
@@ -9,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
